983be96a3e2995094fc8f0f87330c64e92cf85a7
Asit Biswas <asitbiswas870@gmail.com>
2023-07-11 17:16:03 +0530

    add class and onClick to card component

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,8 +1,9 @@
+import { MouseEventHandler } from "react"
 import { DataType } from "../../data"
 import style from "./styles/card.module.css"
-function Card({data}:{data:DataType}) {
+function Card({data,onClick,className}:{data:DataType,onClick?:MouseEventHandler<HTMLDivElement>,className?:string}) {
   return (
-    <div className={style.card}>
+    <div className={`${style.card} ${className||""}`} onClick={onClick}>
         <img className={style.card__img} src={data.url} alt={data.name} />
         <p className={style.card__details}>{data.name}</p>
     </div>
