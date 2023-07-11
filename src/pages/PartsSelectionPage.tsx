bb2e277195356fbd8b87c426415061a827214bd6
Asit Biswas <asitbiswas870@gmail.com>
2023-07-11 17:14:42 +0530

    add part selection page

---
+++ src/pages/PartsSelectionPage.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+import {useNavigate} from "react-router-dom"
+import Card from "../components/card"
+import data from "../data"
+import style from "./styles/partsSelectionPage.module.css"
+
+
+function PartsSelectionPage() {
+    const [pcData,setPcData] = useState(data);
+    const navigate = useNavigate();
+
+
+    function handelClick(i:number){
+        pcData[i] = {...pcData[i],selected:!pcData[i].selected};
+        setPcData([...pcData]);
+    }
+  return (
+    <div className={style.container}>
+        <h1 className={style.heading}>Select Your Pc Parts</h1>
+        <div className={style.card__container}>
+            {
+                pcData.map((el,i)=><Card onClick={()=>handelClick(i)} className={el.selected?style.card__selected:style.card} key={i} data={el}/>)
+            }
+        </div>
+        <button onClick={()=>navigate("/",{state:pcData})} className={style.btn}>Next</button>
+    </div>
+  )
+}
+
+export default PartsSelectionPage
